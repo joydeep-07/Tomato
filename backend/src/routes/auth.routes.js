8b827c0ca597f2ba08ac9d2677b5d3942fbcb7e5
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-08 10:38:32 +0530

    first commit

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.post("/user/register", authController.registerUser);
 router.post('/user/login', authController.loginUser)
+router.get('/user/logout',authController.logoutUser)
 module.exports = router;
