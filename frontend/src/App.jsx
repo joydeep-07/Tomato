295d72215c1d389a15458d700971449c3905d7f6
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-08 12:42:02 +0530

    basic frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-amber-400 bg-amber-100 text-center px-6">
-      <h1 className="text-6xl font-extrabold drop-shadow-lg mb-4">
+      <h1 className="text-6xl font-semibold drop-shadow-lg mb-4">
          Welcome to <span className="text-red-600">Tomato</span>
       </h1>
-      <p className="text-2xl font-medium mb-6">
+      <p className="text-xl font-medium mb-6">
         This website is under construction 🚧
       </p>
      
